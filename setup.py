dd6f3e3e54f8fb247d96ac38bcb67920d3666c02
Kirill Klenov <horneds@gmail.com>
2013-02-27 14:39:11 +0800

    Update version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(fname):
 
 setup(
     name="Inirama",
-    version="0.2.6",
+    version="0.2.7",
     license="BSD",
     description=read('DESCRIPTION'),
     long_description=read('README.rst'),
