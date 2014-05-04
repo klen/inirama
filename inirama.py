000b94ac04d057a5a98d54ddca32a1ed2112b353
Kirill Klenov <horneds@gmail.com>
2014-05-04 23:57:57 +0700

    Update version

--- inirama.py
+++ inirama.py
@@ -66,7 +66,7 @@ except ImportError:
     iteritems = DictMixin.iteritems
 
 
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 __project__ = "Inirama"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
